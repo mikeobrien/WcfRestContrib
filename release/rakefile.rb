08404380f29e0295397039db65343e0b4aaf7ebe
Mike O'Brien <mob@mikeobrien.net>
2010-09-13 14:17:48 -0400

    Build mods.

--- release/rakefile.rb
+++ release/rakefile.rb
@@ -1,7 +1,7 @@
 require "albacore"
 require "release/robocopy"
 
-task :default => [:build]
+task :default => [:zip]
 
 desc "Generate assembly info."
 assemblyinfo :assemblyInfo do |asm|
@@ -14,10 +14,22 @@ assemblyinfo :assemblyInfo do |asm|
   asm.output_file = "src/WcfRestContrib/Properties/AssemblyInfo.cs"
 end
 
+desc "Updates the sample application wcf rest contrib assembly version."
+task :updateSampleVersion
+
+end
+
 desc "Builds the application."
 msbuild :build => [:assemblyInfo] do |msb|
   msb.path_to_command = File.join(ENV['windir'], 'Microsoft.NET', 'Framework', 'v4.0.30319', 'MSBuild.exe')
   msb.properties :configuration => :Release
   msb.targets :Clean, :Build
   msb.solution = "src/WcfRestContrib.sln"
+end
+
+desc "Zips and eploys the application binaries."
+zip => [:build]  do |zip|
+     zip.directories_to_zip "src/WcfRestContrib/bin/Release"
+     zip.output_file = "WcfRestContrib_$(ENV['GO_PIPELINE_LABEL']).zip"
+     zip.output_path = "D:/Websites/public.mikeobrien.net/wwwroot/Releases/WcfRestContrib/"
 end
