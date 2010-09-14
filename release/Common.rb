13f1743f74e3262504fa04b4181f992b96cf7d1c
Mike O'Brien <mob@mikeobrien.net>
2010-09-14 01:35:15 -0400

    Build mods.

--- release/Common.rb
+++ release/Common.rb
@@ -8,6 +8,12 @@ module Common
 		end
 	end
 
+	def Common.DeleteDirectory(path)
+		if Dir.exists?(path) then 
+			 FileUtils.rm_rf path
+		end
+	end
+
 	def Common.CopyFiles(source, target) 
 		Dir.glob(source) do |name|
 			FileUtils.cp(name, target)
--- release/rakefile.rb
+++ release/rakefile.rb
@@ -10,49 +10,48 @@ task :default => [:initDeploy]
 
 desc "Generate assembly info."
 assemblyinfo :assemblyInfo do |asm|
-  asm.version = ENV["GO_PIPELINE_LABEL"]
-  asm.company_name = "Ultraviolet Catastrophe"
-  asm.product_name = "Wcf Rest Contrib"
-  asm.title = "Wcf Rest Contrib"
-  asm.description = "Goodies for Wcf Rest."
-  asm.copyright = "Copyright (c) 2010 Ultraviolet Catastrophe"
-  asm.output_file = "src/WcfRestContrib/Properties/AssemblyInfo.cs"
+    asm.version = ENV["GO_PIPELINE_LABEL"]
+    asm.company_name = "Ultraviolet Catastrophe"
+    asm.product_name = "Wcf Rest Contrib"
+    asm.title = "Wcf Rest Contrib"
+    asm.description = "Goodies for Wcf Rest."
+    asm.copyright = "Copyright (c) 2010 Ultraviolet Catastrophe"
+    asm.output_file = "src/WcfRestContrib/Properties/AssemblyInfo.cs"
 end
 
 desc "Set assembly version in web.config"
 task :setAssemblyVersion => :assemblyInfo do
-	path = "src/NielsBohrLibrary/Web.config"
-	project = Common.ReadAllFileText(path)
-	project = project.gsub("WcfRestContrib, Version=1.0.0.0,", "WcfRestContrib, Version=#{ENV['GO_PIPELINE_LABEL']},")
-	Common.WriteAllFileText(path, project) 
+    path = "src/NielsBohrLibrary/Web.config"
+    project = Common.ReadAllFileText(path)
+    project = project.gsub("WcfRestContrib, Version=1.0.0.0,", "WcfRestContrib, Version=#{ENV['GO_PIPELINE_LABEL']},")
+    Common.WriteAllFileText(path, project) 
 end
 
 desc "Builds the application."
 msbuild :build => :setAssemblyVersion do |msb|
-  msb.path_to_command = File.join(ENV['windir'], 'Microsoft.NET', 'Framework', 'v4.0.30319', 'MSBuild.exe')
-  msb.properties :configuration => :Release
-  msb.targets :Clean, :Build
-  msb.solution = "src/WcfRestContrib.sln"
+    msb.path_to_command = File.join(ENV['windir'], 'Microsoft.NET', 'Framework', 'v4.0.30319', 'MSBuild.exe')
+    msb.properties :configuration => :Release
+    msb.targets :Clean, :Build
+    msb.solution = "src/WcfRestContrib.sln"
 end
 
 desc "Inits the deploy"
 task :initDeploy => :build do
-	puts(ReleasePath)
-	Common.EnsurePath(ReleasePath)
+    Common.EnsurePath(ReleasePath)
 end
 
 desc "Zips and eploys the application binaries."
 zip :deployBinaries => :initDeploy do |zip|
-     zip.directories_to_zip "src/WcfRestContrib/bin/Release"
-     zip.output_file = "WcfRestContrib_#{ENV['GO_PIPELINE_LABEL']}.zip"
-     zip.output_path = ReleasePath
+    zip.directories_to_zip "src/WcfRestContrib/bin/Release"
+    zip.output_file = "WcfRestContrib_#{ENV['GO_PIPELINE_LABEL']}.zip"
+    zip.output_path = ReleasePath
 end
 
 desc "Zips and eploys the application binaries."
 zip :deploySample => :deployBinaries do |zip|
-     zip.directories_to_zip "src/NielsBohrLibrary"
-     zip.output_file = "WcfRestContribSample_#{ENV['GO_PIPELINE_LABEL']}.zip"
-     zip.output_path = ReleasePath
+    zip.directories_to_zip "src/NielsBohrLibrary"
+    zip.output_file = "WcfRestContribSample_#{ENV['GO_PIPELINE_LABEL']}.zip"
+    zip.output_path = ReleasePath
 end
 
 desc "Prepares the gem files to be packaged."
@@ -63,22 +62,15 @@ task :prepareGemFiles => :build do
     docs = "#{gem}/files/docs"
     pkg = "#{gem}/pkg"
     
-    if Dir.exists?(gem) then 
-         FileUtils.rm_rf gem
-    end
+	Common.DeleteDirectory(gem)
+	
+    Common.EnsurePath(lib)
+    Common.EnsurePath(pkg)
+    Common.EnsurePath(docs)
+    
+	Common.CopyFiles("src/WcfRestContrib/bin/Release/*", lib) 
+	Common.CopyFiles("src/docs/**/*", docs) 
 
-    FileUtils.mkdir_p(lib)
-    FileUtils.mkdir_p(pkg)
-    FileUtils.mkdir_p(docs)
-    
-    Dir.glob("src/WcfRestContrib/bin/Release/*") do |name|
-        FileUtils.cp(name, lib)
-    end    
-    
-    Dir.glob("src/docs/**/*") do |name|
-        FileUtils.cp(name, docs)
-    end    
-    
 end
 
 desc "Creates gem"
