a301d21deb1729f4ab751dc583585175254c0a6b
Mike O'Brien <mob@mikeobrien.net>
2010-09-13 21:26:22 -0400

    Build mods.

--- release/rakefile.rb
+++ release/rakefile.rb
@@ -74,6 +74,9 @@ task :prepareGemFiles => :build do
 	
 end
 
+desc "Creates gem"
+task :createGem do
+
 	spec = Gem::Specification.new do |spec|
 		spec.platform = Gem::Platform::RUBY
 		spec.summary = "Goodies for .NET WCF Rest"
@@ -88,11 +91,8 @@ end
 	# Create the Gem package task
 	Rake::GemPackageTask.new(spec) do |package|
 	end
-desc "Creates gem"
-task :createGem do
-
 	
-	Rake::Task["package"].execute
+	Rake::Task["njnljl"].execute
 
 end
 
